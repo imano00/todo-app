f86b654f9af4dc801021c7bd6852e1a314662a7e
imon <nurimanjamal00@gmail.com>
2025-04-18 02:09:45 +0800

    add the task list

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-    <h1>Task List</h1>
+<h1>Task List</h1>
+
+{{-- // Display / Render all of the tasks --}}
+
+@foreach ($tasks as $task)
+
+<div class="card" style="margin-bottom: 20px">
+    <div class="card-body">
+        {{ $task->description }}
+        <a href="btn">Complete</a>
+    </div>
+</div>
+
+@endforeach
+
+<div class="d-grid gap-2">
+    <a href="/tasks/create" class="btn btn-primary btn-lg">New Task</a>
+</div>
 
 @endsection
