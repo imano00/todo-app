0ac656cdcdeb4b9a80bd3c88e7aca959e1565958
imon <nurimanjamal00@gmail.com>
2025-04-17 23:04:46 +0800

    add css link

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+
+        <title>Todo App</title>
+            
+        <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+
+    </head>
+
+    <body>
+
+        <h1>Task List</h1>
+
+    </body>
+
+</html>
