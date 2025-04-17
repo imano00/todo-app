adc9ac71764fceda1a82c21a5ea68157847c7502
imon <nurimanjamal00@gmail.com>
2025-04-18 00:26:59 +0800

    rename button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Iman Todo App</a>
+            <a class="navbar-brand" href="/">Iman Todo App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -20,10 +20,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">All Tasks</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="/tasks/create">Features</a>
+                        <a class="nav-link active" href="/tasks/create">Create</a>
                     </li>
                 </ul>
             </div>
