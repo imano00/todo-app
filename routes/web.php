9fde43ed852773e5d9e50c05aad81202ec205713
imon <nurimanjamal00@gmail.com>
2025-04-17 23:39:51 +0800

    add create route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('tasks.index');
 });
+
+Route::get('/tasks/create', function() {
+    return view('tasks.create');
+});
