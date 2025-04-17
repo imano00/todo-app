5b068de65d8781ceaf6a6487a8951e03383f1ad6
imon <nurimanjamal00@gmail.com>
2025-04-18 03:38:04 +0800

    add styling for completed item

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,19 +2,27 @@
 
 @section('content')
 
-<h1>Task List</h1>
+<h1 style="margin-top: 20px">Task List</h1>
 
 {{-- // Display / Render all of the tasks --}}
 
 @foreach ($tasks as $task)
 
-<div class="card" style="margin-bottom: 20px">
+<div class="card @if($task->isCompleted()) border-success @endif" style="margin-bottom: 20px">
     <div class="card-body">
+
         <p>{{ $task->description }}</p>
+
+        @if($task->isCompleted())
+        <span class="badge rounded-pill text-bg-success">Completed</span>
+        @endif 
+
         <form action="/tasks/{{ $task->id }}" method="POST">
             @method('PATCH')
             @csrf
-            <button class="btn btn-light" input="submit">{{ $task->id }} Complete</button>
+            @if (!$task->isCompleted())
+            <button class="btn btn-light" input="submit">Complete</button>
+            @endif
     </div>
 </div>
 
