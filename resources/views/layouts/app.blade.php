f1a3f87f4d6241603f8876f1889eb1bcae10799e
imon <nurimanjamal00@gmail.com>
2025-04-17 23:32:42 +0800

    add new file to add modular and reusable code

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+    <head>
+
+        <title>Todo App</title>
+            
+        <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+
+</html>
